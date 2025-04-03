38f441a5199277e51a39ca9d6d5c1a16c1783f12
Valifer13 <vianviszello@gmail.com>
2025-04-03 21:06:14 +0800

    create categories table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,12 +14,15 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        $this->call([
+            AdminSeeder::class,
+            CategorySeeder::class,
+        ]);
 
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
 
-        $this->call(AdminSeeder::class);
     }
 }
